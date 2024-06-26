f40c303158e3b7511415d0c9f2e0e6b2649907c5
imran <87297004+imranshaikh9930@users.noreply.github.com>
2024-06-26 18:26:11 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,30 @@
 //your JS code here. If required.
 function flatten(value) {
- return value.flat();
+
+	if(Array.isArray(value)){
+		return value.reduce((acc,item)=>acc.concat(flatten(item)),[]);
+	}
+	else if(typeof value === "object" && value !== null){
+
+		let flattenedObject = {};
+
+		for(let key in value){
+			if(value.hasOwnProperty(key)){
+			    let flatValue = flatten(value[key]);
+				if(typeof flatValue === 'object' && !Array.isAraray(flatValue)){
+					for(let subkey in flatValue){
+						if(flatValue.hasOwnProperty(subkey)){
+							flattenedObject[subkey] = flatValue[subkey];
+						}
+					}
+				}else{
+					flattenedObject[key] = faltValue;
+				}
+			}else{
+				return value;
+			}
+		}
+	}
 }
 
 module.exports=flatten;
