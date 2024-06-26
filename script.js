e6e9ca50a44e7b863e9dd19d3200375b82f47ddc
imran <87297004+imranshaikh9930@users.noreply.github.com>
2024-06-26 18:28:43 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,30 +1,31 @@
 //your JS code here. If required.
 function flatten(value) {
 
-	if(Array.isArray(value)){
-		return value.reduce((acc,item)=>acc.concat(flatten(item)),[]);
-	}
-	else if(typeof value === "object" && value !== null){
-
-		let flattenedObject = {};
-
-		for(let key in value){
-			if(value.hasOwnProperty(key)){
-			    let flatValue = flatten(value[key]);
-				if(typeof flatValue === 'object' && !Array.isAraray(flatValue)){
-					for(let subkey in flatValue){
-						if(flatValue.hasOwnProperty(subkey)){
-							flattenedObject[subkey] = flatValue[subkey];
-						}
-					}
-				}else{
-					flattenedObject[key] = faltValue;
-				}
-			}else{
-				return value;
-			}
-		}
-	}
+	if (Array.isArray(value)) {
+    // Flatten array
+    return value.reduce((acc, item) => acc.concat(flatten(item)), []);
+  } else if (typeof value === 'object' && value !== null) {
+    // Flatten object
+    let flattenedObject = {};
+    for (let key in value) {
+      if (value.hasOwnProperty(key)) {
+        let flatValue = flatten(value[key]);
+        if (typeof flatValue === 'object' && !Array.isArray(flatValue)) {
+          for (let subKey in flatValue) {
+            if (flatValue.hasOwnProperty(subKey)) {
+              flattenedObject[subKey] = flatValue[subKey];
+            }
+          }
+        } else {
+          flattenedObject[key] = flatValue;
+        }
+      }
+    }
+    return flattenedObject;
+  } else {
+    // Return the value if it's not an array or object
+    return value;
+  }
 }
 
 module.exports=flatten;
